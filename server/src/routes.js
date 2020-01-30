6d6a3f4f1c1dd3464a0bde73f563e7b979ad4913
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-01-30 17:16:37 -0300

    Criado rotina para controlar o upload de arquivos

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,14 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.store);
 routes.get('/recipients', RecipientController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
