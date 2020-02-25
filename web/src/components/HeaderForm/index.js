8dbfe45d8239c2cafdb647f724863e33d2f24479
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-02-25 14:47:48 -0300

    Criado componente Header dos Form

---
+++ web/src/components/HeaderForm/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MdKeyboardArrowLeft, MdCheck } from 'react-icons/md';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+import { Header, Title, Component } from './styles';
+
+export default function HeaderList({ title, history }) {
+  function goBack() {
+    history.goBack();
+  }
+  return (
+    <Header>
+      <Title>{title}</Title>
+      <Component>
+        <Button text="VOLTAR" onClick={goBack}>
+          <MdKeyboardArrowLeft size={20} />
+        </Button>
+        <Button confirm text="SALVAR" type="submit" form="form">
+          <MdCheck size={20} />
+        </Button>
+      </Component>
+    </Header>
+  );
+}
+
+HeaderList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onNew: PropTypes.func,
+  find: PropTypes.func.isRequired,
+  buttonNew: PropTypes.bool,
+  search: PropTypes.bool
+};
+
+HeaderList.defaultProps = {
+  buttonNew: true,
+  search: true,
+  onNew: null
+};
---
+++ web/src/components/HeaderForm/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 900px;
+  margin-bottom: 25px;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 24px;
+  color: #444444;
+  margin-bottom: 35px;
+`;
+
+export const Component = styled.div`
+  display: flex;
+`;
