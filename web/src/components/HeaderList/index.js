c784ea49bd32ce15dc531e80aefb72a6e1c8048b
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-02-24 22:47:56 -0300

    Criado componente de header das listas

---
+++ web/src/components/HeaderList/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MdAdd } from 'react-icons/md';
+import PropTypes from 'prop-types';
+import { Form } from '@unform/web';
+import Button from '../Button';
+import { Header, Title, Component, TInput } from './styles';
+
+export default function HeaderList({ title, onNew, find, buttonNew, search }) {
+  return (
+    <Header>
+      <Title>{title}</Title>
+      <Component>
+        {search && <TInput name="search" type="text" placeholder="buscar" />}
+        {buttonNew && (
+          <Button confirm text="CADASTRAR" onClick={onNew}>
+            <MdAdd size={20} />
+          </Button>
+        )}
+      </Component>
+    </Header>
+  );
+}
+
+HeaderList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onNew: PropTypes.func,
+  find: PropTypes.func.isRequired,
+  buttonNew: PropTypes.bool,
+  search: PropTypes.bool
+};
+
+HeaderList.defaultProps = {
+  buttonNew: true,
+  search: true,
+  onNew: null
+};
---
+++ web/src/components/HeaderList/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 940px;
+  align-items: center;
+  margin-bottom: 25aaaapx;
+  align-items: flex-start;
+`;
+
+export const Title = styled.p`
+  font-weight: bold;
+  font-size: 24px;
+  color: #444444;
+  margin-bottom: 35px;
+`;
+
+export const Component = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TInput = styled.input`
+  height: 36px;
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+  padding-left: 10px;
+`;
